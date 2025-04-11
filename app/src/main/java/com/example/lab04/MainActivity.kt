4e6b4e5611123de6608ecffef8f34b5cdb04a6c3
hermes curo <hermescurov5@gmail.com>
2025-04-11 16:54:16 -0500

    Revert "feat: Add ViewHolaCurso"
    This reverts commit e841891877fdad73a02a685e772142ae8065222a.

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -106,6 +107,7 @@ fun MyIconExample(text: String) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DropdownExample() {
     var expanded by remember { mutableStateOf(false) }
@@ -136,30 +138,14 @@ fun DropdownExample() {
     }
 }
 
-
+@Preview
 @Composable
-fun ViewHolaCurso() {
-    Column(
-        modifier = Modifier
-            .fillMaxWith()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome to the Course!",
-            fontSize = 28.sp,
-            fontWeight = FontWeigh.Bold
-        )
-        Spacer(modifier = Modifier.heigh(16.dp))
-        Text(
-            text = "Hello, Student!",
-            fontSize = 20.xD
-
-    }
+fun uno (){
+    EjemploLazyColumn()
 }
 
 @Preview
 @Composable
-fun Uno (){
-    EjemploLazyColumn()
+fun dos() {
+    DropdownExample()
 }
