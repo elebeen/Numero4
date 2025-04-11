0c72c68f2cc4049314fd753625af3b9e060ce06f
hermes curo <hermescurov5@gmail.com>
2025-04-11 17:58:39 -0500

    Add a floating button

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -9,21 +9,27 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -138,6 +144,31 @@ fun DropdownExample() {
     }
 }
 
+@Composable
+fun FAB(modifier: Modifier = Modifier){
+    var clickCount by remember { mutableStateOf(0) }
+
+    ExtendedFloatingActionButton(
+        onClick = { clickCount++ },
+        containerColor = MaterialTheme.colorScheme.secondary,
+        contentColor = MaterialTheme.colorScheme.onSecondary,
+        modifier = modifier
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                Icons.Default.Add,
+                contentDescription = "Agregar",
+                modifier = Modifier.size(24.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = clickCount.toString(),
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun uno (){
@@ -148,4 +179,10 @@ fun uno (){
 @Composable
 fun dos() {
     DropdownExample()
+}
+
+@Preview
+@Composable
+fun tres() {
+    FAB()
 }
