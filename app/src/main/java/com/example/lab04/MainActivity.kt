f46924f2b1a3f22c35ec90b9b1dc438b4f0a21ef
hermes curo <hermescurov5@gmail.com>
2025-04-11 10:27:07 -0500

    add a new functioon
    i added a new function to reuse it in the future

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -7,21 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -63,12 +64,30 @@ fun EjemploLazyColumn() {
                                 .fillMaxSize(),
                             contentAlignment = Alignment.Center
                         ) {
-                            Text("Item $index", textAlign = TextAlign.Center)
+                            MyIconExample(
+                                text = "Item $index"
+                            )
                         }
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MyIconExample(text: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        // Using a custom image from resources:
+        Icon(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "My Custom Icon",
+            modifier = Modifier.size(50.dp) // Adjust size as needed
+        )
+        Text(
+            text = text,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
